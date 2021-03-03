ebf5092fba5f4ec09e2b47037b7c942d0a89f1dd
joshcoronel <joshuajonme@protonmail.com>
2021-03-03 13:46:20 -0500

    with CTE practice

--- Lesson4.sql
+++ Lesson4.sql
@@ -53,3 +53,48 @@ JOIN (
   ORDER BY 3 DESC
 ) t3
   ON t2.region = t3.region AND t2.max_sales = t3.total_sales
+
+--- Provide the name of the sales_rep in each region with the largest amount of total_amt_usd sales.
+WITH rep_sales_total AS (SELECT s.name rep, r.name region, SUM(o.total_amt_usd) total_sales
+      FROM orders o
+      JOIN accounts a
+          ON o.account_id = a.id
+      JOIN sales_reps s
+          ON a.sales_rep_id = s.id
+      JOIN region r
+          ON s.region_id = r.id
+      GROUP BY 1, 2
+      ORDER BY 3 DESC),
+	max_region_sales AS (SELECT region, MAX(total_sales) max_sales
+      FROM rep_sales_total rst
+      GROUP BY region)
+
+SELECT rst.rep, mrs.region, mrs.max_sales
+FROM max_region_sales mrs
+JOIN rep_sales_total rst
+  ON mrs.region = rst.region AND mrs.max_sales = rst.total_sales
+
+--- For the region with the largest sales total_amt_usd, how many total orders were placed?
+WITH region_totals AS (
+		SELECT r.name region_name, SUM(o.total_amt_usd) 	total_amt
+        FROM sales_reps s
+		JOIN accounts a
+			ON a.sales_rep_id = s.id
+		JOIN orders o
+            ON o.account_id = a.id
+		JOIN region r
+            ON r.id = s.region_id
+		GROUP BY r.name)
+
+SELECT r.name, COUNT(o.total) total_orders
+FROM sales_reps s
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN orders o
+ON o.account_id = a.id
+JOIN region r
+ON r.id = s.region_id
+GROUP BY r.name
+HAVING SUM(o.total_amt_usd) = (
+	SELECT MAX(total_amt)
+  FROM region_totals);
