68905ae4a7b927016f07ac61f9919bfccb892222
joshcoronel <joshuajonme@protonmail.com>
2021-03-04 23:51:11 -0500

    completed window function practice

--- Lesson6.sql
+++ Lesson6.sql
@@ -48,3 +48,58 @@ SELECT id,
        MIN(standard_qty) OVER (PARTITION BY account_id) AS min_std_qty,
        MAX(standard_qty) OVER (PARTITION BY account_id) AS max_std_qty
 FROM orders
+
+--- Alias
+SELECT id,
+       account_id,
+       DATE_TRUNC('year',occurred_at) AS year,
+       DENSE_RANK() OVER account_year_window AS dense_rank,
+       total_amt_usd,
+       SUM(total_amt_usd) OVER account_year_window AS sum_total_amt_usd,
+       COUNT(total_amt_usd) OVER account_year_window AS count_total_amt_usd,
+       AVG(total_amt_usd) OVER account_year_window AS avg_total_amt_usd,
+       MIN(total_amt_usd) OVER account_year_window AS min_total_amt_usd,
+       MAX(total_amt_usd) OVER account_year_window AS max_total_amt_usd
+FROM orders
+WINDOW account_year_window AS (PARTITION BY account_id ORDER BY DATE_TRUNC('year',occurred_at))
+
+
+--- Comparing a Row to Previous Row
+SELECT occurred_at,
+       total,
+       LEAD(total) OVER (ORDER BY total) AS lead,
+       LEAD(total) OVER (ORDER BY total) - total AS lead_difference
+FROM (
+    SELECT occurred_at,
+        SUM(total_amt_usd) AS total
+    FROM orders 
+    GROUP BY 1
+ ) sub
+
+ --- Percentiles
+ --- Use the NTILE functionality to divide the accounts into 4 levels in terms of the amount of standard_qty for their orders. Your resulting table should have the account_id, the occurred_at time for each order, the total amount of standard_qty paper purchased, and one of four levels in a standard_quartile column.
+ SELECT 
+	account_id,
+    occurred_at,
+    standard_qty,
+    NTILE(4) OVER (ORDER BY standard_qty) standard_quartile
+FROM orders
+ORDER BY 3 DESC;
+
+--- Use the NTILE functionality to divide the accounts into two levels in terms of the amount of gloss_qty for their orders. Your resulting table should have the account_id, the occurred_at time for each order, the total amount of gloss_qty paper purchased, and one of two levels in a gloss_half column.
+SELECT 
+	account_id,
+    occurred_at,
+    gloss_qty,
+    NTILE(2) OVER (ORDER BY gloss_qty) gloss_half
+FROM orders
+ORDER BY 3 DESC;
+
+--- Use the NTILE functionality to divide the orders for each account into 100 levels in terms of the amount of total_amt_usd for their orders. Your resulting table should have the account_id, the occurred_at time for each order, the total amount of total_amt_usd paper purchased, and one of 100 levels in a total_percentile column.
+SELECT 
+	account_id,
+    occurred_at,
+    total_amt_usd,
+    NTILE(100) OVER (ORDER BY total_amt_usd) total_percentile
+FROM orders
+ORDER BY 1 DESC;
